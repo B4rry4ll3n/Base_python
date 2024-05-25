0054fecfd7eb12f2d4d818b5723837d5b0157861
Isaias Junior <86421873+B4rry4ll3n@users.noreply.github.com>
2024-05-25 09:12:47 -0300

    update
    update

---
+++ Python/exercicios1.py
@@ -0,0 +1,26 @@
+if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+
+#     if par_impar:
+#         par_impar_texto = 'par'
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
+
+try:
+    entrada_int = float(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
+
+    if par_impar:
+        par_impar_texto = 'par'
+
+    print(f'O número {entrada_int} é {par_impar_texto}')
+except:
+    print('Você não digitou um número inteiro')
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
